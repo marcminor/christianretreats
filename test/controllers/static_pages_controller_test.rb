59a759af97095d6cfb38e0e05f0788d68e3560c1
Marc Minor <mminor@apple.com>
2015-01-10 19:18:38 -0800

    Add page titles & tests to static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,19 +2,26 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
+	def setup
+		@base_title = "Multitude - Christian Retreats"
+	end
+	
 	test "should get home" do
 		get :home
 		assert_response :success
+		assert_select "title", "Home | #{@base_title}"
 	end
 
 	test "should get about" do 
 		get :about
 		assert_response :success
+		assert_select "title", "About | #{@base_title}"
 	end
 
 	test "should get help" do 
 		get :contact
 		assert_response :success
+		assert_select "title", "Help | #{@base_title}"
 	end
 
 end
