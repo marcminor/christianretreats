4b5f811c7f1087dfc59bb9a1bd720db862a5ad6e
Marc Minor <marceminor@gmail.com>
2015-01-12 15:12:58 -0800

    create application helper test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,6 +10,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select	"a[href=?]", contact_path, count: 2
 		assert_match "List Your Retreat", response.body
 		assert_select	"a[href=?]", about_path
+		get signup_path
+    assert_select "title", full_title("Sign up")
 	end
 
 	test "unique headers" do 
